f6e5be295cd82d3b65a3d260c703b27c62614b26
Lashvardi <lashakakashvili114@gmail.com>
2023-05-04 14:43:21 +0400

    F

--- Javascript #2/main.js
+++ Javascript #2/main.js
@@ -102,3 +102,8 @@ const colors = ["red", "blue", "green", "yellow"];
 for (let i = 0; i < colors.length; i++) {
   console.log(colors[i]);
 }
+
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+  }

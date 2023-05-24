6df0b8a62636cd72b0af5b51afdb110be1e79794
Lasha <lashakakashvili114@gmail.com>
2023-05-24 21:08:49 +0400

    Minore

--- Javascript #4/main5.js
+++ Javascript #4/main5.js
@@ -9,6 +9,7 @@ console.log(thirdArray); // Output: [1, 2, 3, 'four', true, 5, 7, 12, 5125, 25]
 let myArrayString = myArray.join(", ");
 console.log(myArrayString); // Output: "1, 2, 3, four, true"
 
+
 // ? Reverse მოატრიალებს ჩვენს მასივს
 myArray.reverse();
 console.log(myArray); // Output: [true, 'four', 3, 2, 1]
@@ -66,37 +67,86 @@ console.log(person.hobbies);
 person.jobTitle = "Web Developer";
 console.log(person);
 
-let employees = [
+// let employees = [
+//   {
+//     name: "John Doe",
+//     age: 30,
+//     hobbies: ["reading", "cooking", "hiking"],
+//     address: {
+//       street: "123 Main St",
+//     },
+//   },
+//   {
+//     name: "Jane Smith",
+//     age: 25,
+//     hobbies: ["swimming", "traveling", "dancing"],
+//     address: {
+//       street: "456 Elm St",
+//     },
+//   },
+//   {
+//     name: "Bob Johnson",
+//     age: 40,
+//     hobbies: ["fishing", "camping", "gardening"],
+//     address: {
+//       street: "789 Oak St",
+//     },
+//   },
+// ];
+
+// let box = document.getElementById("test1");
+
+// for (let i = 0; i < employees.length; i++) {
+//     box.innerHTML += <h1>employees[i]</h1>;
+// }
+
+
+let Cars = [
   {
-    name: "John Doe",
-    age: 30,
-    hobbies: ["reading", "cooking", "hiking"],
-    address: {
-      street: "123 Main St",
-    },
+    img: "https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNhcnN8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+    name: "BMW",
+    model: "X5",
+    color: "Black",
+    price: 50000
   },
   {
-    name: "Jane Smith",
-    age: 25,
-    hobbies: ["swimming", "traveling", "dancing"],
-    address: {
-      street: "456 Elm St",
-    },
+    img: "https://images.unsplash.com/photo-1552519507-da3b142c6e3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcnN8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+    name: "Mercedes",
+    model: "S500",
+    color: "Blue",
+    price: 60000
   },
   {
-    name: "Bob Johnson",
-    age: 40,
-    hobbies: ["fishing", "camping", "gardening"],
-    address: {
-      street: "789 Oak St",
-    },
+    img: "https://img.freepik.com/premium-photo/blue-premium-business-sedan-car-sports-configuration-white-background-3d-rendering_101266-26564.jpg",
+    name: "Audi",
+    model: "A8",
+    color: "Blue",
+    price: 70000
   },
-];
+  {
+    img: "https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/128413/scorpio-exterior-right-front-three-quarter-46.jpeg?isig=0&q=75",
+    name: "Mahindra",
+    model: "Scorpio",
+    color: "Red",
+    price: 40000
+  }
+]
+
 
 let box = document.getElementById("test1");
 
-for (let i = 0; i < employees.length; i++) {
-    box.innerHTML += `<h1>${employees[i].name}</h1>`;
+for (let i = 0; i < Cars.length; i++) {
+  let card = `
+  <div class="card" style="width: 18rem;">
+  <img src="${Cars[i].img}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${Cars[i].name}</h5>
+    <p>${Cars[i].model}</p>
+    <p style="color: ${Cars[i].color}">${Cars[i].color}</p>
+    <a href="#" class="btn btn-primary">${Cars[i].price}$</a>
+  </div>
+</div>`
+box.innerHTML += card;
 }
 
 
--- Javascript #5/main.js
+++ Javascript #5/main.js
@@ -1,31 +1,26 @@
 const numbers = [1, 2, 3, 4, 5];
-const squaredNumbers = numbers.map(num => num * num);
-console.log(squaredNumbers); // Output: [1, 4, 9, 16, 25]
+const kvadratuliRicxvebi = numbers.map((cifri) => cifri * cifri);
+console.log(kvadratuliRicxvebi); // Output: [1, 4, 9, 16, 25]
 
+const numbers1 = [1, 2, 3, 4, 5, 6];
+const luwebi = numbers1.filter((num) => num % 2 != 0);
+console.log(luwebi); // Output: [2, 4]
 
-const numbers1 = [1, 2, 3, 4, 5];
-const evenNumbers = numbers1.filter(num => num % 2 === 0);
-console.log(evenNumbers); // Output: [2, 4]
+const wignebi = [
+  { title: "Harry Potter", author: "J.K. Rowling", year: 1997 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  { title: "1984", author: "George Orwell", year: 1949 },
+];
+const bookTitles = wignebi.map((book) => book.year);
+console.log(bookTitles);
 
-
-const books = [
-    { title: 'Harry Potter', author: 'J.K. Rowling' },
-    { title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-    { title: '1984', author: 'George Orwell' }
-  ];
-const bookTitles = books.map(book => book.title);
-console.log(bookTitles); // Output: ['Harry Potter', 'To Kill a Mockingbird', '1984']
-  
-
-const words = ['apple', 'banana', 'orange', 'grape'];
-const shortWords = words.filter(word => word.length < 6);
+const words = ["apple", "banana", "orange", "grape"];
+const shortWords = words.filter((word) => word.length < 6);
 console.log(shortWords); // Output: ['apple', 'grape']
 
-
 const numbers3 = [1, 2, 3, 4, 5];
-const numberStrings = numbers3.map(num => num.toString());
+const numberStrings = numbers3.map((num) => num.toString());
 console.log(numberStrings); // Output: ['1', '2', '3', '4', '5']
-
 
 // ! Map()
 // ? ჯავასკრიპტში Map() მეთოდი იღებს რაღაც წესს რასაც მოერგება შემდეგ ყველა ელემენტს გადაუვლის და დაგვიბრუნებს
@@ -34,37 +29,23 @@ console.log(numberStrings); // Output: ['1', '2', '3', '4', '5']
 //! Filter()
 //? ჯავასკრიპტში Filter() მეთოდი ქმნის ახალ Arrays რომელიც აკმაყოფილებს ჩვენს მიერ გადაცემულ წესს
 
-
-
 //! Javascript Math()
-
-
 //? ხარისხად
-const xarisxi = Math.pow(2, 3);
+const xarisxi = Math.pow(2, 6);
 console.log(xarisxi); // Output: 8
 
-
-
 //? ფესვი
-const fesvi = Math.sqrt(64);
+const fesvi = Math.sqrt(190);
 console.log(fesvi); // Output: 8
 
 const dadebiti = Math.abs(-4.7);
 console.log(dadebiti); // Output: 4.7
 
-
-const maximumi = Matn.max(0, 150, 30, 20, -8, -200);
+const maximumi = Math.max(0, 150, 30, 20, -8, -200, 1000);
 console.log(maximumi); // Output: 150
-
 
 const minimumi = Math.min(0, 150, 30, 20, -8, -200);
 console.log(minimumi); // Output: -200
 
-
 const random = Math.random();
 console.log(random); // Output: 0.9362944283730668
-
-
-
-  
-
